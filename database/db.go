b35f8b6c841e8910677049eb2262c88408af7d5f
cryptonceo <149706114+cryptonceo@users.noreply.github.com>
2025-07-04 06:50:48 +0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -54,108 +54,48 @@ func InitDB(dsn string) error {
 }
 
 func initModels() error {
-	models := []any{
+	return db.AutoMigrate(
 		&model.User{},
 		&model.Inbound{},
-		&model.OutboundTraffics{},
 		&model.Setting{},
+		&model.ClientTraffic{},
 		&model.InboundClientIps{},
-		&xray.ClientTraffic{},
-		&model.HistoryOfSeeders{},
-	}
-	for _, model := range models {
-		if err := db.AutoMigrate(model); err != nil {
-			log.Printf("Error auto migrating model: %v", err)
-			return err
-		}
-	}
-	return nil
-}
-
-func initUser() error {
-	empty, err := isTableEmpty("users")
-	if err != nil {
-		log.Printf("Error checking if users table is empty: %v", err)
-		return err
-	}
-	if empty {
-		hashedPassword, err := crypto.HashPasswordAsBcrypt(defaultPassword)
-		if err != nil {
-			log.Printf("Error hashing default password: %v", err)
-			return err
-		}
-		user := &model.User{
-			Username: defaultUsername,
-			Password: hashedPassword,
-		}
-		return db.Create(user).Error
-	}
-	return nil
-}
-
-func runSeeders(isUsersEmpty bool) error {
-	empty, err := isTableEmpty("history_of_seeders")
-	if err != nil {
-		log.Printf("Error checking if history_of_seeders table is empty: %v", err)
-		return err
-	}
-
-	if empty && isUsersEmpty {
-		hashSeeder := &model.HistoryOfSeeders{
-			SeederName: "UserPasswordHash",
-		}
-		return db.Create(hashSeeder).Error
-	} else {
-		var seedersHistory []string
-		db.Model(&model.HistoryOfSeeders{}).Pluck("seeder_name", &seedersHistory)
-
-		if !slices.Contains(seedersHistory, "UserPasswordHash") && !isUsersEmpty {
-			var users []model.User
-			db.Find(&users)
-
-			for _, user := range users {
-				hashedPassword, err := crypto.HashPasswordAsBcrypt(user.Password)
-				if err != nil {
-					log.Printf("Error hashing password for user '%s': %v", user.Username, err)
-					return err
-				}
-				db.Model(&user).Update("password", hashedPassword)
-			}
-
-			hashSeeder := &model.HistoryOfSeeders{
-				SeederName: "UserPasswordHash",
-			}
-			return db.Create(hashSeeder).Error
-		}
-	}
-	return nil
+	)
 }
 
 func isTableEmpty(tableName string) (bool, error) {
 	var count int64
 	err := db.Table(tableName).Count(&count).Error
-	return count == 0, err
+	if err != nil {
+		return false, err
+	}
+	return count == 0, nil
 }
 
-func CloseDB() error {
-	if db != nil {
-		sqlDB, err := db.DB()
-		if err != nil {
-			return err
+func initUser() error {
+	var count int64
+	err := db.Model(&model.User{}).Count(&count).Error
+	if err != nil {
+		return err
+	}
+
+	if count == 0 {
+		user := model.User{
+			Username: defaultUsername,
+			Password: crypto.SHA256(defaultPassword),
 		}
-		return sqlDB.Close()
+		return db.Create(&user).Error
+	}
+	return nil
+}
+
+func runSeeders(isUsersEmpty bool) error {
+	if isUsersEmpty {
+		log.Println("Running seeders for initial data...")
 	}
 	return nil
 }
 
 func GetDB() *gorm.DB {
 	return db
-}
-
-func IsNotFound(err error) bool {
-	return err == gorm.ErrRecordNotFound
-}
-
-func Checkpoint() error {
-	return nil
 }
