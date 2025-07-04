c2b441300243884e6ed17ae8e0e33e4b4f4da46c
cryptonceo <149706114+cryptonceo@users.noreply.github.com>
2025-07-04 19:45:16 +0300

    Update model.go

--- database/model/model.go
+++ database/model/model.go
@@ -27,41 +27,81 @@ type User struct {
 }
 
 type Inbound struct {
-	Id           int            `json:"id" gorm:"primaryKey;autoIncrement"`
-	UserId       int            `json:"userId"`
-	Up           int64          `json:"up"`
-	Down         int64          `json:"down"`
-	Total        int64          `json:"total"`
-	Remark       string         `json:"remark"`
-	Enable       bool           `json:"enable"`
-	ExpiryTime   int64          `json:"expiryTime"`
-	ClientStats  json_util.RawMessage `json:"clientStats" gorm:"type:json"`
-	Listen       string         `json:"listen"`
-	Port         int            `json:"port"`
-	Protocol     Protocol       `json:"protocol"`
-	Settings     string         `json:"settings"`
-	StreamSettings string       `json:"streamSettings"`
-	Tag          string         `json:"tag"`
-	Sniffing     string         `json:"sniffing"`
+	Id          int                  `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
+	UserId      int                  `json:"-"`
+	Up          int64                `json:"up" form:"up"`
+	Down        int64                `json:"down" form:"down"`
+	Total       int64                `json:"total" form:"total"`
+	Remark      string               `json:"remark" form:"remark"`
+	Enable      bool                 `json:"enable" form:"enable"`
+	ExpiryTime  int64                `json:"expiryTime" form:"expiryTime"`
+	ClientStats []xray.ClientTraffic `gorm:"foreignKey:InboundId;references:Id" json:"clientStats" form:"clientStats"`
+
+	// config part
+	Listen         string   `json:"listen" form:"listen"`
+	Port           int      `json:"port" form:"port"`
+	Protocol       Protocol `json:"protocol" form:"protocol"`
+	Settings       string   `json:"settings" form:"settings"`
+	StreamSettings string   `json:"streamSettings" form:"streamSettings"`
+	Tag            string   `json:"tag" form:"tag" gorm:"unique"`
+	Sniffing       string   `json:"sniffing" form:"sniffing"`
+	Allocate       string   `json:"allocate" form:"allocate"`
 }
 
-type Setting struct {
-	Id    int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
-
-type ClientTraffic struct {
-	Id     int   `json:"id" gorm:"primaryKey;autoIncrement"`
-	InboundId int `json:"inboundId"`
-	Email     string `json:"email"`
-	Up        int64  `json:"up"`
-	Down      int64  `json:"down"`
+type OutboundTraffics struct {
+	Id    int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
+	Tag   string `json:"tag" form:"tag" gorm:"unique"`
+	Up    int64  `json:"up" form:"up" gorm:"default:0"`
+	Down  int64  `json:"down" form:"down" gorm:"default:0"`
+	Total int64  `json:"total" form:"total" gorm:"default:0"`
 }
 
 type InboundClientIps struct {
-	Id        int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	InboundId int    `json:"inboundId"`
-	Email     string `json:"email"`
-	Ips       string `json:"ips"`
+	Id          int    `json:"id" gorm:"primaryKey;autoIncrement"`
+	ClientEmail string `json:"clientEmail" form:"clientEmail" gorm:"unique"`
+	Ips         string `json:"ips" form:"ips"`
+}
+
+type HistoryOfSeeders struct {
+	Id         int    `json:"id" gorm:"primaryKey;autoIncrement"`
+	SeederName string `json:"seederName"`
+}
+
+func (i *Inbound) GenXrayInboundConfig() *xray.InboundConfig {
+	listen := i.Listen
+	if listen != "" {
+		listen = fmt.Sprintf("\"%v\"", listen)
+	}
+	return &xray.InboundConfig{
+		Listen:         json_util.RawMessage(listen),
+		Port:           i.Port,
+		Protocol:       string(i.Protocol),
+		Settings:       json_util.RawMessage(i.Settings),
+		StreamSettings: json_util.RawMessage(i.StreamSettings),
+		Tag:            i.Tag,
+		Sniffing:       json_util.RawMessage(i.Sniffing),
+		Allocate:       json_util.RawMessage(i.Allocate),
+	}
+}
+
+type Setting struct {
+	Id    int    `json:"id" form:"id" gorm:"primaryKey;autoIncrement"`
+	Key   string `json:"key" form:"key"`
+	Value string `json:"value" form:"value"`
+}
+
+type Client struct {
+	ID         string `json:"id"`
+	Security   string `json:"security"`
+	Password   string `json:"password"`
+	Flow       string `json:"flow"`
+	Email      string `json:"email"`
+	LimitIP    int    `json:"limitIp"`
+	TotalGB    int64  `json:"totalGB" form:"totalGB"`
+	ExpiryTime int64  `json:"expiryTime" form:"expiryTime"`
+	Enable     bool   `json:"enable" form:"enable"`
+	TgID       int64  `json:"tgId" form:"tgId"`
+	SubID      string `json:"subId" form:"subId"`
+	Comment    string `json:"comment" form:"comment"`
+	Reset      int    `json:"reset" form:"reset"`
 }
